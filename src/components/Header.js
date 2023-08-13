d2ae7447dc5bded3d7bfb3675886b96831236de8
hanielchids <hanielchids@gmail.com>
2023-08-13 21:48:49 +0200

    config files added

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        {/* top nav */}
+        <div>
+            <div>
+            <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={150}
+            />
+            </div>
+
+        </div>
+
+        {/* bottom nav */}
+        <div>
+
+        </div>
+
+    </header>
+  )
+}
+
+export default Header
